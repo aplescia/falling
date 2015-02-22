e147589456d3ca24110af1eb8111ec39f9326f80
aplescia <anthony.plescia@mail.mcgill.ca>
2015-02-22 10:57:19 -0500

    initial commit

---
+++ core/src/com/falling/game/MyGdxGame.java
@@ -0,0 +1,61 @@
+package com.falling.game;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.lang.Math;
+
+public class MyGdxGame implements ApplicationListener {
+		private Texture texture;
+	    private SpriteBatch batch;
+	    private BitmapFont font;
+	    private Sprite sprite;
+	    private Texture texturetwo;
+	    
+	    @Override
+	    public void create() {        
+	        batch = new SpriteBatch();    
+	        font = new BitmapFont();
+	        /*two textures just to test, one for red circle and one for purple circle*/
+	        texture = new Texture(Gdx.files.internal("redcircle.png"));
+	        texturetwo = new Texture(Gdx.files.internal("purpcircle.png"));
+	        
+	      //probably an easier way to randomly generate a sprite...not that far yet. might need to use an atlas/spritemap
+	        double theRandom = Math.random();
+	        sprite = new Sprite(texture);
+	    }
+
+	    @Override
+	    public void dispose() {
+	        batch.dispose();
+	        font.dispose();
+	    }
+
+	    @Override
+	    public void render() {        
+	        Gdx.gl.glClearColor(1, 1, 1, 1);
+	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	        
+	        batch.begin();
+	        sprite.draw(batch);
+	        batch.end();
+	    }
+
+	    @Override
+	    public void resize(int width, int height) {
+	    }
+
+	    @Override
+	    public void pause() {
+	    }
+
+	    @Override
+	    public void resume() {
+	    }
+	}
+
---
+++ desktop/src/com/falling/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.falling.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.falling.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
